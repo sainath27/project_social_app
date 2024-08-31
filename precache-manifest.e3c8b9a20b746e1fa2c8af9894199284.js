1c02b8cfb5f4f3913543214ea09f723d7a0abe2d
chirumamilla sainath <chirumamillasainath@gmail.com>
2024-08-31 11:00:31 +0530

    Updates

---
+++ precache-manifest.e3c8b9a20b746e1fa2c8af9894199284.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b611b3036e5487757cf2c93acbcd1bf6",
+    "url": "/project_social_app/index.html"
+  },
+  {
+    "revision": "527c9f40991aad2c4462",
+    "url": "/project_social_app/static/css/main.58095ad1.chunk.css"
+  },
+  {
+    "revision": "395be05d07ec675b6b61",
+    "url": "/project_social_app/static/js/2.afeb26e6.chunk.js"
+  },
+  {
+    "revision": "c87ae3f2a437a1b2df8d",
+    "url": "/project_social_app/static/js/3.177faaa1.chunk.js"
+  },
+  {
+    "revision": "527c9f40991aad2c4462",
+    "url": "/project_social_app/static/js/main.6ad8a65f.chunk.js"
+  },
+  {
+    "revision": "6dc3152b01fbed10aa5f",
+    "url": "/project_social_app/static/js/runtime~main.d7a6fbd1.js"
+  }
+]);
